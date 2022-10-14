49e2d2079ac9bfb83df285bf0cda742d31755a93
rszimmer <rdszimmer@inf.ufpel.edu.br>
2022-10-14 11:02:50 -0300

    Le mais de um frame por vez

--- fullSearch.cpp
+++ fullSearch.cpp
@@ -2,15 +2,16 @@
 #include <cstdlib>
 #include <omp.h>
 
-void readFrames(FILE *fp, unsigned char **frame1, unsigned char **frame2, int width, int height);
+void readFrame(FILE *fp, unsigned char **frame, int width, int height);
 int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned char **Rv, unsigned char **Ra);
 
 int main(int argc, char *argv[])
 {
     int width = 640;
     int height = 360;
-    unsigned char **frame1;
-    unsigned char **frame2;
+    unsigned char **frameRef;
+    unsigned char **frameAtual;
+
     unsigned char **Rv;
     unsigned char **Ra;
     int maxBlocks = width * height / 64;
@@ -19,8 +20,8 @@ int main(int argc, char *argv[])
     
     begin = omp_get_wtime();
 
-    frame1 = (unsigned char **)malloc(sizeof *frame1 * height);
-    frame2 = (unsigned char **)malloc(sizeof *frame2 * height);
+    frameRef = (unsigned char **)malloc(sizeof *frameRef * height);
+    frameAtual = (unsigned char **)malloc(sizeof *frameAtual * height);
 
     FILE *fp = fopen("video_converted_640x360.yuv", "rb");
 
@@ -29,52 +30,51 @@ int main(int argc, char *argv[])
         printf("Cannot open file");
     }
 
-    readFrames(fp, frame1, frame2, width, height);
+    readFrame(fp, frameRef, width, height);
 
-    // alocar os vetore Rv e Ra
-    Rv = (unsigned char **)malloc(sizeof *Rv * maxBlocks);
-    Ra = (unsigned char **)malloc(sizeof *Ra * maxBlocks);
+    for (int frameI=0; frameI < 10 ; frameI++){
+        readFrame(fp, frameAtual, width, height);
 
-    #pragma omp parallel
-    {
-        size = fullSearch(frame1, frame2, Rv, Ra);
+        // alocar os vetore Rv e Ra
+        Rv = (unsigned char **)malloc(sizeof *Rv * maxBlocks);
+        Ra = (unsigned char **)malloc(sizeof *Ra * maxBlocks);
+
+        #pragma omp parallel
+        {
+            size = fullSearch(frameRef, frameAtual, Rv, Ra);
+        }
+
+        // for (int i = 0; i < size; i++) {
+        //     printf("Ra: (%d, %d)\n", Ra[i][0], Ra[i][1]);
+        //     printf("Rv: (%d, %d)\n\n", Rv[i][0], Rv[i][1]);
+        // }
     }
-    
     end = omp_get_wtime();
     // Close file
     fclose(fp);
 
-    for (int i = 0; i < size; i++) {
-        printf("Ra: (%d, %d)\n", Ra[i][0], Ra[i][1]);
-        printf("Rv: (%d, %d)\n\n", Rv[i][0], Rv[i][1]);
-    }
     printf("Tempo de execução: %.2f segundos\n", end-begin);
 }
 
-void readFrames(FILE *fp, unsigned char **frame1, unsigned char **frame2, int width, int height)
+
+void readFrame(FILE *fp, unsigned char **frame, int width, int height)
 {
     int i;
     unsigned char *temp;
 
     temp = (unsigned char *)malloc(sizeof *temp * width * height);
 
-    // Read Y frame 1
+    // Read Y frame
     for (i = 0; i < height; i++)
     {
-        frame1[i] = (unsigned char *)malloc(sizeof *frame1[i] * width);
-        fread(frame1[i], sizeof(unsigned char), width, fp);
+        frame[i] = (unsigned char *)malloc(sizeof *frame[i] * width);
+        fread(frame[i], sizeof(unsigned char), width, fp);
     }
 
     // Skip CbCR channels
     fread(temp, sizeof(unsigned char), width * height / 2, fp);
-
-    // Read Y frame 2
-    for (i = 0; i < height; i++)
-    {
-        frame2[i] = (unsigned char *)malloc(sizeof *frame2[i] * width);
-        fread(frame2[i], sizeof(unsigned char), width, fp);
-    }
 }
+
 
 int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned char **Rv, unsigned char **Ra)
 {
@@ -83,8 +83,6 @@ int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned char **R
     int height = 360;
 
     int position = 0;
-
-    int encontrou = 0;
 
     int totalDifference = 0;
     int minTotalDifference = 16500;
@@ -96,7 +94,7 @@ int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned char **R
     int skip = 0;
 
     // percorre blocos do frame 2 (atual)
-    # pragma omp for collapse(2)
+    # pragma omp for collapse(2) nowait
     for (i = 0; i < height; i = i+8) {
         for (j = 0; j < width; j = j+8) {
             minTotalDifference = 16500;
@@ -128,25 +126,21 @@ int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned char **R
                 }
             }
 
-            if (minTotalDifference <= differenceTreshold) {
-                    // guarda nos vetores
-                    Rv[position] = (unsigned char *)malloc(sizeof *Rv[i] * 2);
-                    Rv[position][0] = minK;
-                    Rv[position][1] = minL;
+            // guarda bloco com menor diferenca nos vetores
+            Rv[position] = (unsigned char *)malloc(sizeof *Rv[i] * 2);
+            Rv[position][0] = minK;
+            Rv[position][1] = minL;
 
-                    Ra[position] = (unsigned char *)malloc(sizeof *Ra[i] * 2);
-                    Ra[position][0] = i;
-                    Ra[position][1] = j;
+            Ra[position] = (unsigned char *)malloc(sizeof *Ra[i] * 2);
+            Ra[position][0] = i;
+            Ra[position][1] = j;
 
-                    // printf("Ra [%d]: (%d, %d) -> %d\n", position, Ra[position][0], Ra[position][1], minTotalDifference);
-                    // printf("Rv [%d]: (%d, %d) -> %d\n\n", position, Rv[position][0], Rv[position][1], minTotalDifference);
+            // printf("Ra [%d]: (%d, %d) -> %d\n", position, Ra[position][0], Ra[position][1], minTotalDifference);
+            // printf("Rv [%d]: (%d, %d) -> %d\n\n", position, Rv[position][0], Rv[position][1], minTotalDifference);
 
-                    position++;
-                }
+            position++;
         }
     }
-    // se encontrar guardar nos vetores Rv e Ra
-    // fazer isso pra todos os frames seguintes até o fim
 
     return position;
  }
