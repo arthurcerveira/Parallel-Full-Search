02fdfe728f8c87113ef7bc4c58a12b41603cfe41
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2022-10-20 19:07:33 -0300

    Salva resultado em arquivo binário

--- fullSearch.cpp
+++ fullSearch.cpp
@@ -2,24 +2,28 @@
 #include <cstdlib>
 #include <omp.h>
 
+using namespace std;
+
 void readFrame(FILE *fp, unsigned char **frame, int width, int height);
 int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv, unsigned int **Ra);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int width = 640;
     int height = 360;
     unsigned char **frameRef;
     unsigned char **frameAtual;
+
+    // Array of frames
+    unsigned char ***frames;
+    int nFrames = 5;
+    int frameI;
 
     unsigned int **Rv;
     unsigned int **Ra;
     int maxBlocks = width * height / 64;
     int size;
     double begin, end;
-    omp_set_num_threads(5);
-    
-    begin = omp_get_wtime();
+    omp_set_num_threads(1);
 
     frameRef = (unsigned char **)malloc(sizeof *frameRef * height);
     frameAtual = (unsigned char **)malloc(sizeof *frameAtual * height);
@@ -31,20 +35,47 @@ int main(int argc, char *argv[])
         printf("Cannot open file");
     }
 
+    // Read frame 1 as reference
     readFrame(fp, frameRef, width, height);
 
-    #pragma omp parallel for shared(frameAtual, fp, width, height, maxBlocks) private(size) lastprivate(Ra, Rv)
-        for (int frameI=0; frameI < 5 ; frameI++){
-            printf("Inicio frame %d\n", frameI);
+    // Read remaining frames and store in array
+    frames = (unsigned char ***)malloc(sizeof ***frames * nFrames);
 
-            readFrame(fp, frameAtual, width, height);
+    for (frameI = 0; frameI < nFrames; frameI++) {
+        frames[frameI] = (unsigned char **)malloc(sizeof *frames[frameI]);
+        readFrame(fp, frames[frameI], width, height);
+    }
 
-            // alocar os vetore Rv e Ra
+    begin = omp_get_wtime();
+    
+    // For each frame, run fullSearch
+    #pragma omp parallel for shared(frames, fp, width, height, maxBlocks) private(size) lastprivate(Ra, Rv)
+        for (frameI=0; frameI < nFrames ; frameI++){
+            printf("Inicio frame %d. Thread %d\n", frameI, omp_get_thread_num());
+
+            // Rv and Ra store the results of fullSearch
             Rv = (unsigned int **)malloc(sizeof *Rv * maxBlocks);
             Ra = (unsigned int **)malloc(sizeof *Ra * maxBlocks);
-            size = fullSearch(frameRef, frameAtual, Rv, Ra);
+            size = fullSearch(frameRef, frames[frameI], Rv, Ra);
 
-            printf("Fim frame %d: %d blocos movimentados\n", frameI, size);
+            // Write results to file
+            char* fileName = new char[20];
+            snprintf(fileName, 12, "%d.bin", frameI);
+
+            FILE * result = fopen(fileName, "wb");
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < 2; j++) {
+                    fwrite(&Ra[i][j], sizeof(Ra[i][j]), 1, result);
+                    fwrite(&Rv[i][j], sizeof(Rv[i][j]), 1, result);
+                }
+                // fwrite(&Ra[i], sizeof(Ra[i]), 1, result);
+                // fwrite(&Rv[i], sizeof(Rv[i]), 1, result);
+            }
+
+            fclose(result);
+
+            printf("Fim frame %d: %d blocos processados\n", frameI, size);
         }
 
     end = omp_get_wtime();
@@ -54,7 +85,47 @@ int main(int argc, char *argv[])
     // Close file
     fclose(fp);
 
-    for (int i = 0; i < 100; i++) {
+    // Concatenate results into a single file
+    FILE * result = fopen("coded_video.bin", "wb");
+
+    // Write first frame to file
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            fwrite(&frameRef[i][j], sizeof(frameRef[i][j]), 1, result);
+        }
+    }
+
+    // Write remaining frames to file
+    for (frameI = 0; frameI < nFrames; frameI++) {
+        char* fileName = new char[20];
+
+        snprintf(fileName, 12, "%d.bin", frameI);
+
+        FILE * partialResult = fopen(fileName, "rb");
+
+        // fseek(partialResult, 0, SEEK_END);
+        // size = ftell(partialResult) / 8;
+        // fseek(partialResult, 0, SEEK_SET);
+        
+        // There is an X and Y value for each block
+        size = (maxBlocks - 1) * 2;
+
+        for (int i = 0; i < size; i++) {
+            unsigned int Ra, Rv;
+
+            fread(&Ra, sizeof(Ra), 1, partialResult);
+            fread(&Rv, sizeof(Rv), 1, partialResult);
+
+            fwrite(&Ra, sizeof(Ra), 1, result);
+            fwrite(&Rv, sizeof(Rv), 1, result);
+        }
+
+        fclose(partialResult);
+
+        remove(fileName);
+    }
+    
+    for (int i = 0; i < 10; i++) {
         printf("Ra: [%d] (%d, %d)\n", i, Ra[i][0], Ra[i][1]);
         printf("Rv: [%d] (%d, %d)\n\n", i, Rv[i][0], Rv[i][1]);
     }
@@ -63,8 +134,7 @@ int main(int argc, char *argv[])
 }
 
 
-void readFrame(FILE *fp, unsigned char **frame, int width, int height)
-{
+void readFrame(FILE *fp, unsigned char **frame, int width, int height) {
     int i;
     unsigned char *temp;
 
@@ -82,8 +152,7 @@ void readFrame(FILE *fp, unsigned char **frame, int width, int height)
 }
 
 
-int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv, unsigned int **Ra)
-{
+int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv, unsigned int **Ra) {
     int i, j, k, l, m, n, aux = 0;
     int posI, posJ;
     int width = 640;
@@ -94,11 +163,7 @@ int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv
     int totalDifference = 0;
     int minTotalDifference = 16500;
 
-    int differenceTreshold = 100;
-
     int minK, minL;
-
-    int skip = 0;
 
     // percorre blocos do frame 2 (atual)
     // # pragma omp for collapse(2) nowait
