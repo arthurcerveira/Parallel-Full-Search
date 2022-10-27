903e8906798f3e76e363b51fc0186afb796785e7
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2022-10-27 12:16:36 -0300

    Corrige leitura e limpa comentários

--- fullSearch.cpp
+++ fullSearch.cpp
@@ -5,17 +5,16 @@
 using namespace std;
 
 void readFrame(FILE *fp, unsigned char **frame, int width, int height);
-int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv, unsigned int **Ra);
+int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv, unsigned int **Ra, int frameI);
 
 int main(int argc, char *argv[]) {
     int width = 640;
     int height = 360;
     unsigned char **frameRef;
-    unsigned char **frameAtual;
 
-    // Array of frames
+    // Array de frames
     unsigned char ***frames;
-    int nFrames = 5;
+    int nFrames = 20;
     int frameI;
 
     unsigned int **Rv;
@@ -23,10 +22,10 @@ int main(int argc, char *argv[]) {
     int maxBlocks = width * height / 64;
     int size;
     double begin, end;
+    
     omp_set_num_threads(8);
 
     frameRef = (unsigned char **)malloc(sizeof *frameRef * height);
-    frameAtual = (unsigned char **)malloc(sizeof *frameAtual * height);
 
     FILE *fp = fopen("video_converted_640x360.yuv", "rb");
 
@@ -35,31 +34,35 @@ int main(int argc, char *argv[]) {
         printf("Cannot open file");
     }
 
-    // Read frame 1 as reference
+    // Lê frame 1 como referencia
     readFrame(fp, frameRef, width, height);
 
-    // Read remaining frames and store in array
+    // Lê quadros restante e guarda em array
     frames = (unsigned char ***)malloc(sizeof ***frames * nFrames);
 
     for (frameI = 0; frameI < nFrames; frameI++) {
-        frames[frameI] = (unsigned char **)malloc(sizeof *frames[frameI]);
+        frames[frameI] = (unsigned char **)malloc(sizeof *frames[frameI] * height);
         readFrame(fp, frames[frameI], width, height);
     }
 
-    begin = omp_get_wtime();
+    // begin = omp_get_wtime();
+
+    // printf("Início da região paralela\n");
     
-    // For each frame, run fullSearch
+    // Para cada quadro, executa fullSearch
     #pragma omp parallel for shared(frames, fp, width, height, maxBlocks) private(size) lastprivate(Ra, Rv)
         for (frameI=0; frameI < nFrames ; frameI++){
-            printf("Inicio frame %d. Thread %d\n", frameI, omp_get_thread_num());
+            printf("Inicio frame %d.\n", frameI);
+            // printf("Thread %d\n", omp_get_thread_num());
 
-            // Rv and Ra store the results of fullSearch
+            // Rv e Ra guardam resultados do fullSearch
             Rv = (unsigned int **)malloc(sizeof *Rv * maxBlocks);
             Ra = (unsigned int **)malloc(sizeof *Ra * maxBlocks);
-            size = fullSearch(frameRef, frames[frameI], Rv, Ra);
+            size = fullSearch(frameRef, frames[frameI], Rv, Ra, frameI);
 
-            // Write results to file
+            // Escreve esses resultados em um arquivo binário
             char* fileName = new char[20];
+            // Gambiarra para definir nome do arquivo
             snprintf(fileName, 12, "%d.bin", frameI);
 
             FILE * result = fopen(fileName, "wb");
@@ -69,55 +72,47 @@ int main(int argc, char *argv[]) {
                     fwrite(&Ra[i][j], sizeof(Ra[i][j]), 1, result);
                     fwrite(&Rv[i][j], sizeof(Rv[i][j]), 1, result);
                 }
-                // fwrite(&Ra[i], sizeof(Ra[i]), 1, result);
-                // fwrite(&Rv[i], sizeof(Rv[i]), 1, result);
             }
 
             fclose(result);
-
-            printf("Fim frame %d: %d blocos processados\n", frameI, size);
         }
 
-    end = omp_get_wtime();
+    // end = omp_get_wtime();
 
-    printf("Fim da região paralela\n");
+    // printf("Fim da região paralela\n");
     
-    // Close file
+    // Fecha video
     fclose(fp);
 
-    // Concatenate results into a single file
+    // Concatena resultados em um único arquivo
     FILE * result = fopen("coded_video.bin", "wb");
 
-    // Write first frame to file
+    // Escreve primeiro quadro no arquivo
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             fwrite(&frameRef[i][j], sizeof(frameRef[i][j]), 1, result);
         }
     }
 
-    // Write remaining frames to file
+    // Escreve resultados do fullSearch para outros quadros
     for (frameI = 0; frameI < nFrames; frameI++) {
         char* fileName = new char[20];
 
         snprintf(fileName, 12, "%d.bin", frameI);
 
         FILE * partialResult = fopen(fileName, "rb");
-
-        // fseek(partialResult, 0, SEEK_END);
-        // size = ftell(partialResult) / 8;
-        // fseek(partialResult, 0, SEEK_SET);
         
-        // There is an X and Y value for each block
+        // Tem um valor X e Y para cada bloco
         size = (maxBlocks - 1) * 2;
 
         for (int i = 0; i < size; i++) {
-            unsigned int Ra, Rv;
+            unsigned int RaI, RvI;
 
-            fread(&Ra, sizeof(Ra), 1, partialResult);
-            fread(&Rv, sizeof(Rv), 1, partialResult);
+            fread(&RaI, sizeof(RaI), 1, partialResult);
+            fread(&RvI, sizeof(RvI), 1, partialResult);
 
-            fwrite(&Ra, sizeof(Ra), 1, result);
-            fwrite(&Rv, sizeof(Rv), 1, result);
+            fwrite(&RaI, sizeof(RaI), 1, result);
+            fwrite(&RvI, sizeof(RvI), 1, result);
         }
 
         fclose(partialResult);
@@ -125,12 +120,12 @@ int main(int argc, char *argv[]) {
         remove(fileName);
     }
     
-    // for (int i = 0; i < maxBlocks; i++) {
-    //     printf("Ra: [%d] (%d, %d)\n", i, Ra[i][0], Ra[i][1]);
-    //     printf("Rv: [%d] (%d, %d)\n\n", i, Rv[i][0], Rv[i][1]);
-    // }
+    for (int i = 0; i < 5; i++) {
+        printf("Ra: [%d] (%d, %d)\n", i, Ra[i][0], Ra[i][1]);
+        printf("Rv: [%d] (%d, %d)\n\n", i, Rv[i][0], Rv[i][1]);
+    }
 
-    printf("Tempo de execução: %.2f segundos\n", end-begin);
+    // printf("Tempo de execução: %.2f segundos\n", end-begin);
 }
 
 
@@ -140,20 +135,20 @@ void readFrame(FILE *fp, unsigned char **frame, int width, int height) {
 
     temp = (unsigned char *)malloc(sizeof *temp * width * height);
 
-    // Read Y frame
+    // Lê frame Y
     for (i = 0; i < height; i++)
     {
         frame[i] = (unsigned char *)malloc(sizeof *frame[i] * width);
         fread(frame[i], sizeof(unsigned char), width, fp);
     }
 
-    // Skip CbCR channels
+    // Pula canais CbCR
     fread(temp, sizeof(unsigned char), width * height / 2, fp);
 }
 
 
-int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv, unsigned int **Ra) {
-    int i, j, k, l, m, n, aux = 0;
+int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv, unsigned int **Ra, int frameI) {
+    int i, j, k, l, m, n = 0;
     int posI, posJ, posK, posL;
     int width = 640;
     int height = 360;
@@ -165,38 +160,34 @@ int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv
 
     int minK, minL;
 
-    // percorre blocos do frame 2 (atual)
-    //# pragma omp for collapse(2) nowait
+    // Percorre blocos do frame atual
     for (i = 0; i < height/8; i++) {
-        for (j = 0; j < width/8; j++) {
+        for (j = 0; j < width/8; j++) {            
             posI = i*8;
             posJ = j*8;
+
             minTotalDifference = 16500;
             minK = 0;
             minL = 0;
 
-            //printf("%d\n", omp_get_thread_num());
-
-            // percorre blocos do frame 1 (referencia)
-            // Parallel For
-            #pragma omp for collapse(2) nowait
+            // Percorre blocos do frame 1 (referencia)
+            #pragma omp for collapse(2) nowait schedule(static)
             for (k = 0; k < height/8; k++) {
                 for (l = 0; l < width/8; l++) {
                     totalDifference = 0;
-                    posK = i*8;
-                    posL = j*8;
+                    posK = k*8;
+                    posL = l*8;
 
                     // percorre pixels do bloco de referencia
-                    // Parallel for com reduction para o totalDifference
-                        //# pragma omp for collapse(2)
-                        for (m = 0; m < 8; m++) {
-                            for (n = 0; n < 8; n++) {
-                                // compara pixels do frame atual com referencia
-                                totalDifference += abs(frame2[posI+m][posJ+n] - frame1[posK+m][posL+n]);
-                            }
+                    for (m = 0; m < 8; m++) {
+                        for (n = 0; n < 8; n++) {
+                            // compara pixels do frame atual com referencia
+                            totalDifference += abs(
+                                frame2[posI+m][posJ+n] - frame1[posK+m][posL+n]
+                            );
                         }
+                    }
 
-                    // Seção critica de atualizar o minTotalDifference
                     if (totalDifference < minTotalDifference) {
                         minTotalDifference = totalDifference;
                         minK = posK;
@@ -205,7 +196,7 @@ int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv
                 }
             }
 
-            // guarda bloco com menor diferenca nos vetores
+            // Guarda bloco com menor diferenca nos vetores
             position = (i * width / 8) + j;
             
             Rv[position] = (unsigned int *)malloc(sizeof *Rv[position] * 2);
@@ -215,10 +206,6 @@ int fullSearch(unsigned char **frame1, unsigned char **frame2, unsigned int **Rv
             Ra[position] = (unsigned int *)malloc(sizeof *Ra[position] * 2);
             Ra[position][0] = posI;
             Ra[position][1] = posJ;
-            
-            // printf("Ra: [%d] (%d, %d)\n", position, Ra[position][0], Ra[position][1]);
-            // printf("Rv: [%d] (%d, %d)\n", position, Rv[position][0], Rv[position][1]);
-            // printf("%d\n\n", omp_get_thread_num());
         }
     }
     return position;
